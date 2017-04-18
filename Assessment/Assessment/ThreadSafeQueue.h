087486c49ba4daea7ffa63ca7265ce1bdde0e6a0
Narek Atayan <narek_atayan@epam.com>
2017-04-18 18:43:39 +0400

    Implementation of lock-based thread-safe queue.

---
+++ Assessment/Assessment/ThreadSafeQueue.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <queue>
+#include <condition_variable>
+#include <shared_mutex>
+
+namespace Parallel
+{
+
+template<typename T>
+class ThreadSafeQueue
+{
+public: // public interface
+  typedef typename std::queue<T>::value_type value_type;
+  typedef typename std::queue<T>::size_type  size_type;
+
+public:
+  ThreadSafeQueue(size_type maxSize = 1000);
+
+  void push(const value_type v);
+  void pop(value_type& v);
+
+  bool empty() const;
+  auto size() const;
+
+private: // members
+  std::queue<value_type>   dataQueue_;
+  mutable std::shared_mutex mutex_;
+
+  std::condition_variable_any  cVForProducers_;
+  std::condition_variable_any  cVForConsumers_;
+  bool                         reachedToMax_;
+  size_type                    maxSize_;
+};
+
+}
---
+++ Assessment/Assessment/ThreadSafeQueue_Impl.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include "ThreadSafeQueue.h"
+#include <mutex> // for unique_lock
+
+namespace Parallel
+{
+typedef std::unique_lock<std::shared_mutex> UniqueLock;
+typedef std::shared_lock<std::shared_mutex> SharedLock;
+
+
+template<typename T>
+ThreadSafeQueue<T>::ThreadSafeQueue(size_type maxSize)
+    : dataQueue_()
+    , mutex_()
+    , cVForProducers_()
+    , cVForConsumers_()
+    , reachedToMax_(false)
+    , maxSize_(maxSize)
+{}
+
+template<typename T>
+void
+ThreadSafeQueue<T>::push(const value_type v)
+{
+    UniqueLock lck(mutex_);
+
+    cVForProducers_.wait(lck, [this] {
+        if (dataQueue_.size() >= maxSize_)
+            reachedToMax_ = true;
+
+        return !reachedToMax_;
+    });
+
+    dataQueue_.push(v);
+
+    cVForConsumers_.notify_all();
+}
+
+
+template<typename T>
+void
+ThreadSafeQueue<T>::pop(value_type& v)
+{
+    UniqueLock lck(mutex_);
+
+    cVForConsumers_.wait(lck, [this] {
+        return !dataQueue_.empty();
+    });
+
+    v = dataQueue_.front();
+    dataQueue_.pop();
+
+    if (!reachedToMax_ && dataQueue_.size() < maxSize_)
+        cVForProducers_.notify_all();
+}
+
+template<typename T>
+bool
+ThreadSafeQueue<T>::empty() const
+{
+    SharedLock lck(mutex_);
+    return dataQueue_.empty();
+}
+
+template<typename T>
+auto
+ThreadSafeQueue<T>::size() const
+{
+    SharedLock lck(mutex_);
+    return dataQueue_.size();
+}
+
+}
